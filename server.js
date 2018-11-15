317b73f90b5da1a65cddbf1894dca7f4256351a3
atul92cs <atul.11192@gmail.com>
2018-11-15 21:17:39 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+var express=require('express');
+var mongoose=require('mongoose');
+var path=require('path');
+var bodyParser=require('body-parser');
+var routes=require('./routes');
+var app=express();
+var port=process.env.PORT||3001;
+app.set('views', __dirname + '/Views');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/',routes.home);
+app.listen(port, () => {
+    console.log('server started on' + ' ' + port);
+});
